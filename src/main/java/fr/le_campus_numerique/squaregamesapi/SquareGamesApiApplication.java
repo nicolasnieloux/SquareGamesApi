d151654f27890136dc48dd58421855da48a913de
nicolasnieloux <nicolas.nieloux@le-campus-numerique.fr>
2024-02-16 11:18:15 +0100

    first commit

---
+++ src/main/java/fr/le_campus_numerique/squaregamesapi/SquareGamesApiApplication.java
@@ -0,0 +1,13 @@
+package fr.le_campus_numerique.squaregamesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareGamesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SquareGamesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/le_campus_numerique/squaregamesapi/SquareGamesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.le_campus_numerique.squaregamesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareGamesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
